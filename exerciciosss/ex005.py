82350d273728d40aaad62e6cd34154bb8efb9600
Felipe da Silva Borba <borbafelipe007@gmail.com>
2023-05-08 19:59:11 -0300

    Criação ex 005 e ex 006

---
+++ exerciciosss/ex005.py
@@ -0,0 +1,5 @@
+n=int(input('Digite um número: '))
+dobro=n**2
+triplo=n**3
+raiz=n**0.5
+print('Valor escolhido {} \n Seu dobro {} \n Seu triplo {} \n Sua raiz {}'.format(n,dobro,triplo,raiz))
---
+++ exerciciosss/ex006.py
@@ -0,0 +1,4 @@
+n1=float(input('Digite a primeira nota: '))
+n2=float(input('Digite a Segunda nota: '))
+media=(n1+n2)/2
+print('Nota primeira prova {} \n Nota segunda prova \n Média {:.1f}'.format(n1,n2,media))
